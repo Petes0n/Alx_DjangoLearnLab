758eedd7e7329838ba8924bad8d61a62a1a1faf9
Pete <peterdaayeng@gmail.com>
2026-01-21 17:18:44 +0000

    relationships

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,3 +9,10 @@ def list_books(request):
     return render(request, "relationship_app/list_books.html", {
         "books": books
     })
+
+
+# Class-based view: library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"

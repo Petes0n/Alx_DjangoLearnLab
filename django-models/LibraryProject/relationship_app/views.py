525c002d292aac7b31ea26163ebe4e0767ada9f9
Pete <peterdaayeng@gmail.com>
2026-01-21 17:28:11 +0000

    relationships

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 
 # Function-based view: list all books
@@ -17,3 +21,25 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+# User login view
+class UserLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+
+# User logout view
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+
+# User registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html", {"form": form})

7e43ef7fe1acac1413ff0b7c30f80f7cc45ea110
Pete <peterdaayeng@gmail.com>
2026-01-26 17:31:06 +0000

    new admin2

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,10 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 
-# Register your models here.
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional Information", {
-            "fields": ("date_of_birth", "profile_photo"),
-        }),
-    )
+    pass
+
+
+admin.site.register(CustomUser, CustomUserAdmin)

c181da3b1a4b65b0c96c806e03d3704f5abdabce
Pete <peterdaayeng@gmail.com>
2026-01-26 17:16:21 +0000

    new

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -9,3 +10,11 @@ class Book(models.Model):
         return self.title
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    # objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
